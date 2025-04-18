dbea6e796e1f1268836b0b2cc03afe9109e4cbbf
Pradip Shrestha <pradipshrestha718@gmail.com>
2025-04-18 16:36:17 +0545

    WIP: Report Block Scale

--- src/screens/VoiceScan/Reports/index.tsx
+++ src/screens/VoiceScan/Reports/index.tsx
@@ -1,8 +1,7 @@
-import {RouteProp} from '@react-navigation/native';
 import {entries, isEmpty, isObject} from 'lodash';
 import moment from 'moment';
 import {View} from 'moti';
-import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {
   FlatList,
   SafeAreaView,
@@ -12,14 +11,14 @@ import {
 } from 'react-native';
 import useLanguageStore from '../../../../store/languageStore';
 import useVoiceScanStore from '../../../../store/voiceScanStore';
+import {VoiceScanReport as VoiceScanReportType} from '../../../../types/api_response';
 import BasicContainer from '../../../components/BasicContainer';
+import Icon from '../../../components/Icon';
 import Navbar from '../../../components/Navbar';
 import RenderReport from '../../../components/RenderReport';
+import ReportCard from '../../../components/ReportCard';
 import CustomText from '../../../components/Text';
-
-import Icon from '../../../components/Icon';
 import customColor from '../../../theme/customColor';
-import VoiceScanReportConfidence from './components/ReportConfidence';
 import WellnessScore from './components/WellnessScore';
 
 const RenderDateAndTitle = ({date}: any) => {
@@ -37,106 +36,14 @@ const RenderDateAndTitle = ({date}: any) => {
   );
 };
 
-// const RenderReportInformation = ({
-//   readingId,
-// }: {
-//   // reportData: ReportJsonResponse;
-//   readingId: string;
-// }) => {
-//   const [expandedSections, setExpandedSections] = useState<string[]>([]);
-//   const [isLoading, setIsLoading] = useState(true);
-
-//   const {data: reportData} = useGetUserReadingDetail({
-//     reading_id: readingId,
-//     // staleTime: Infinity,
-//     gcTime: 0,
-//   });
-
-//   const latestReading = useMemo(() => {
-//     return reportData?.readings;
-//   }, [reportData?.readings]);
-
-//   useEffect(() => {
-//     setExpandedSections(Object.keys(reportData?.sub_categorisation || {}));
-//     const timer = setTimeout(() => setIsLoading(false), 500); // Simulate loading delay
-//     return () => clearTimeout(timer);
-//   }, [reportData]);
-
-//   const {reading_data} = latestReading || {};
-
-//   const toggleSection = useCallback((key: string) => {
-//     setExpandedSections(prevState =>
-//       prevState.includes(key)
-//         ? prevState.filter(section => section !== key)
-//         : [...prevState, key],
-//     );
-//   }, []);
-
-//   const readingsConfidence = useMemo(() => {
-//     return reading_data
-//       ? Object.entries(reading_data).filter(
-//           ([_key, value]) => value.confidence_level,
-//         )
-//       : undefined;
-//   }, [reading_data]);
-
-//   const renderCardItem = useCallback(
-//     (param: Parameter) => (
-//       <View key={param.vital_key}>
-//         {reading_data?.[param.vital_key] &&
-//           (param.vital_key === 'BLOOD_PRESSURE' ? (
-//             <RenderMultiReport
-//               readingObj={reading_data.BLOOD_PRESSURE}
-//               readingKey="BLOOD_PRESSURE"
-//               reading={latestReading}
-//             />
-//           ) : (
-//             <RenderReport
-//               reading={latestReading}
-//               readingKey={param.vital_key}
-//               subParameters={param.subParameters}
-//               name={param.display}
-//             />
-//           ))}
-//       </View>
-//     ),
-//     [reading_data, latestReading],
-//   );
-
-//   if (isLoading) {
-//     return (
-//       <View className="p-6">
-//         <ReportSkeleton />
-//       </View>
-//     );
-//   }
-
-//   return (
-//     <View>
-//       <ReportConfidence
-//         classNameValue="mt-8 mx-4"
-//         readingId={readingId}
-//         readingsConfidence={readingsConfidence}
-//         overallConfidence={reportData?.readings?.confidence_level ?? ''}
-//       />
-//       {isObject(reportData?.sub_categorisation) && (
-//         <FlatList
-//           data={entries(reportData?.sub_categorisation)}
-//           keyExtractor={item => item[0]}
-//           renderItem={renderItem}
-//           scrollEnabled={false}
-//         />
-//       )}
-//     </View>
-//   );
-// };
-
 const VoiceScanReport = () => {
   const {reportDetail} = useVoiceScanStore();
   const [expandedSections, setExpandedSections] = useState<string[]>([]);
 
   useEffect(() => {
-    setExpandedSections(Object.keys(reportDetail?.sub_categorization || {}));
+    if (reportDetail?.sub_categorization) {
+      setExpandedSections(Object.keys(reportDetail.sub_categorization));
+    }
   }, [reportDetail]);
 
   const toggleSection = useCallback((key: string) => {
@@ -147,29 +54,63 @@ const VoiceScanReport = () => {
     );
   }, []);
 
+  const renderReportItem = useCallback(
+    (reportItem: VoiceScanReportType['voice_scan_report'][0]) => {
+      const config = reportDetail?.scale_config?.[reportItem.key];
+      console.log('config', config);
+      if (!config) return null;
+
+      const scaleCriteria = {
+        scale: config?.scale || [],
+        measuredValue: reportItem?.value ?? 0,
+      };
+
+      return (
+        <ReportCard
+          readingId={''}
+          scaleType={config?.scale_type ?? 1}
+          colorRange={config?.color_range ?? []}
+          // colorRange={[]}
+          healthMetricsTitle={config?.display}
+          healthMetricsValue={reportItem?.value || 0}
+          healthMetricsIndex={config?.unit ?? ''}
+          iconName={reportItem?.key}
+          description={config?.short_info ?? ''}
+          score={Number(reportItem?.value) || 0}
+          category={reportItem?.category ?? ''}
+          color_value={null}
+          scaleCriteria={scaleCriteria}
+          confidenceLevel={'High'}
+          subParameters={[]}
+          // onDetailsPress={handleDetailsPress}
+          onDetailsPress={() => {}}
+        />
+      );
+    },
+    [reportDetail],
+  );
+
   const renderItem = useCallback(
-    ({item: [vitalKey, params]}: {item: [string, string[]]}) => {
-      console.log('item', vitalKey, params);
-      if (
-        isEmpty(params) ||
-        !params.some(param =>
-          reportDetail?.voice_scan_report?.some(
-            eachReport => eachReport?.key === param,
-          ),
-        )
-      ) {
+    ({item: [category, keys]}: {item: [string, string[]]}) => {
+      if (isEmpty(keys) || !reportDetail?.voice_scan_report) {
         return null;
       }
-      const isExpanded = expandedSections.includes(vitalKey);
+
+      const isExpanded = expandedSections.includes(category);
+      const categoryReports = reportDetail.voice_scan_report.filter(report =>
+        keys.includes(report.key),
+      );
+
+      if (categoryReports.length === 0) return null;
+
       return (
         <View>
           <TouchableOpacity
             className="border-b py-4 border-[#868686] px-2 flex-row justify-between items-center"
-            onPress={() => toggleSection(vitalKey)}>
+            onPress={() => toggleSection(category)}>
             <CustomText className="text-midnight text-base font-isidoraSemiBold">
-              {vitalKey}
+              {category}
             </CustomText>
-
             <Icon
               name={isExpanded ? 'remove' : 'add'}
               size={isExpanded ? 8 : 20}
@@ -177,25 +118,21 @@ const VoiceScanReport = () => {
               className="px-4 self-center"
             />
           </TouchableOpacity>
-          {isExpanded &&
-            params.map(param => (
-              <View key={param}>
-                {reading_data?.[param.vital_key] && (
-                  <RenderReport
-                    reading={latestReading}
-                    readingKey={param.vital_key}
-                    subParameters={param.subParameters}
-                    name={param.display}
-                  />
-                )}
-              </View>
-            ))}
+          {isExpanded && categoryReports.map(renderReportItem)}
         </View>
       );
     },
-    // [expandedSections, toggleSection, renderCardItem, reading_data, reportData],
-    [],
+    [
+      expandedSections,
+      toggleSection,
+      renderReportItem,
+      reportDetail?.voice_scan_report,
+    ],
   );
+
+  if (!reportDetail) {
+    return null;
+  }
 
   return (
     <BasicContainer className="bg-white">
@@ -207,15 +144,15 @@ const VoiceScanReport = () => {
           contentContainerStyle={styles.scrollviewContentContainer}
           className="h-full">
           <View>
-            <WellnessScore score={reportDetail?.wellness_score ?? 0} />
+            <WellnessScore score={reportDetail.wellness_score} />
           </View>
           <View className="bg-white mt-[-30px] rounded-t-3xl flex-1 pt-4">
-            <RenderDateAndTitle date={reportDetail?.report_generation_time} />
+            <RenderDateAndTitle date={reportDetail.report_generation_time} />
 
             <View>
-              {isObject(reportDetail?.sub_categorization) && (
+              {isObject(reportDetail.sub_categorization) && (
                 <FlatList
-                  data={entries(reportDetail?.sub_categorization)}
+                  data={entries(reportDetail.sub_categorization)}
                   keyExtractor={item => item[0]}
                   renderItem={renderItem}
                   scrollEnabled={false}
