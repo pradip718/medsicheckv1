e5aacbdab7eaf4ea1aa2ae46a68cf47d54761a6d
Pradip Shrestha <pradipshrestha718@gmail.com>
2025-06-11 15:25:42 +0545

    PP link change

--- android/app/build/generated/source/buildConfig/production/release/mx/medsi/medsicheck/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/production/release/mx/medsi/medsicheck/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "mx.medsi.medsicheck";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "production";
-  public static final int VERSION_CODE = 69;
-  public static final String VERSION_NAME = "4.3.4";
+  public static final int VERSION_CODE = 70;
+  public static final String VERSION_NAME = "4.3.5";
   // Field from default config.
   public static final String BASE_URL = "https://medsicheck-api.medsi.ai/";
   // Field from default config.
