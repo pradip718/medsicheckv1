5634ed80f38b46e243fabc9affe5eef1a2bef5b5
Pradip Shrestha <pradipshrestha718@gmail.com>
2025-06-11 16:33:08 +0545

    build dev apk

---
+++ android/app/build/generated/source/buildConfig/development/release/mx/medsi/medsicheck/BuildConfig.java
@@ -0,0 +1,23 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package mx.medsi.medsicheck;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "mx.medsi.medsicheck.dev";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "development";
+  public static final int VERSION_CODE = 70;
+  public static final String VERSION_NAME = "4.3.5";
+  // Field from default config.
+  public static final String BASE_URL = "https://dev.medsicheck-api.medsi.ai/";
+  // Field from default config.
+  public static final String Environment = "development";
+  // Field from default config.
+  public static final boolean IS_HERMES_ENABLED = true;
+  // Field from default config.
+  public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
+  // Field from default config.
+  public static final String NO_FLIPPER = "1";
+}
