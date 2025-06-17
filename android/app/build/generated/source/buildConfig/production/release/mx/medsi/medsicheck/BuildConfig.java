7630ceeb85baa4d465bc2213ede45b914b06755d
Pradip Shrestha <pradipshrestha718@gmail.com>
2025-06-17 18:17:13 +0545

    Clean up tracked build artifacts and apply .gitignore

--- android/app/build/generated/source/buildConfig/production/release/mx/medsi/medsicheck/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/production/release/mx/medsi/medsicheck/BuildConfig.java
@@ -8,7 +8,7 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "mx.medsi.medsicheck";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "production";
-  public static final int VERSION_CODE = 70;
+  public static final int VERSION_CODE = 71;
   public static final String VERSION_NAME = "4.3.5";
   // Field from default config.
   public static final String BASE_URL = "https://medsicheck-api.medsi.ai/";
