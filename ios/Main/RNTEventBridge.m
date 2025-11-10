58e440d8cd8555a4fb7588f1681b1d4ee164c2b6
Pradip Shrestha <pradipshrestha718@gmail.com>
2025-11-10 18:13:45 +0545

    Increase android build number

--- ios/Main/RNTEventBridge.m
+++ ios/Main/RNTEventBridge.m
@@ -14,8 +14,8 @@
 
 #import "RNTEventBridge.h"
 //#import "MedsiCheckDev-Swift.h" 
-//#import "MedsiCheck-Swift.h"
- #import "MedsiCheckStaging-Swift.h"
+#import "MedsiCheck-Swift.h"
+// #import "MedsiCheckStaging-Swift.h"
 
 NSString *const EventReminderCommon = @"eventReminder_common";
 NSString *const EventReminderResults = @"eventReminder_results";
