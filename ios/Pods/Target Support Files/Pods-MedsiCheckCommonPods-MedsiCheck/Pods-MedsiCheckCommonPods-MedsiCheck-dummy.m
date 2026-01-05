186a34aa3a216e51c35fd8ceb83c4dc67985de09
Pradip Shrestha <pradipshrestha718@gmail.com>
2026-01-05 12:55:53 +0545

    remove pods from git tracking

--- ios/Pods/Target Support Files/Pods-MedsiCheckCommonPods-MedsiCheck/Pods-MedsiCheckCommonPods-MedsiCheck-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MedsiCheckCommonPods_MedsiCheck : NSObject
-@end
-@implementation PodsDummy_Pods_MedsiCheckCommonPods_MedsiCheck
-@end
--- ios/Pods/Target Support Files/Pods-MedsiCheckCommonPods-MedsiCheckTests/Pods-MedsiCheckCommonPods-MedsiCheckTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MedsiCheckCommonPods_MedsiCheckTests : NSObject
-@end
-@implementation PodsDummy_Pods_MedsiCheckCommonPods_MedsiCheckTests
-@end
